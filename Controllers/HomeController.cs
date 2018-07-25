bf786801faf30d6c16ab7cbd9da186d543a0da53
Ali Bayati <bayati.ali@icloud.com>
2018-07-25 12:29:29 -0500

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using crypto.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using System;
+using Newtonsoft.Json;
+using System.Web;
+using System.Net.Http;
+using System.Threading.Tasks;
+namespace crypto.Controllers
+{
+    public class CryptoController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult> Index()
+        {
+            //Trim lat and lon
+            var bitcoin = await Models.CryptoProxy.GetCryptoPrice();
+            ViewBag.BitPrice = bitcoin.data.amount;
+            ViewBag.Bitcoin = bitcoin.data.currency;
+            return View("Request");
+        }
+    }
+}
